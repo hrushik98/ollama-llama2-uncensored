c6e2ae0bb1310058bf37a76fb178d33aed28a434
Hrushik <91076764+hrushik98@users.noreply.github.com>
2024-03-24 20:14:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
         try:
             response = send_api_request(api_url, payload)
             st.subheader("Response")
-            st.json(response)
+            st.json(response)['response']
         except Exception as e:
             st.error(f"Error: {e}")
 
